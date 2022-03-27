ab642c30cda5f46a86ebfbadd605e992661857ad
dd84ai <dd84ai@gmail.com>
2022-03-27 19:23:31 +0700

    feat: generating post data

--- src/psql/architecture/task_2_architecture.sql
+++ src/psql/architecture/task_2_architecture.sql
@@ -9,12 +9,12 @@ DROP TABLE IF EXISTS post_visits_per_day;
 CREATE TABLE users
 (
   	id SERIAL PRIMARY KEY,
-  	first_name CHAR(100) NOT NULL,
-	second_name CHAR(100) NOT NULL,
+  	first_name VARCHAR(100) NOT NULL,
+	second_name VARCHAR(100) NOT NULL,
   	birth_date DATE NOT NULL,
-	email CHAR(100) NOT NULL,
-	password CHAR(100) NOT NULL,
-	address CHAR(200) NOT NULL,
+	email VARCHAR(100) NOT NULL,
+	password VARCHAR(100) NOT NULL,
+	address VARCHAR(200) NOT NULL,
     rating INTEGER DEFAULT 0 -- Auto calculated by TRIGGER
 );
 
@@ -151,94 +151,19 @@ CREATE TRIGGER user_rating_trigger_3
 		EXECUTE PROCEDURE user_rating_trigger_function();
 
 CREATE INDEX
+  ON posts (id);
+
+CREATE INDEX
   ON posts (author_id);
+
+CREATE INDEX
+  ON post_editions (post_id);
 
 CREATE INDEX
   ON posts (created_at);
 
--- add to users, posts, post_approvals
-DO $$
-  DECLARE v_users_number INT;
-  DECLARE v_posts_for_each_user INT;
-BEGIN
- v_users_number := 2000;
- v_posts_for_each_user := 100;
+CREATE INDEX
+  ON posts USING GIN (tags);
 
-  INSERT INTO users(first_name, second_name, birth_date, email, password, address) SELECT
-    concat('first_name', num),
-    concat('second_name', num),
-    DATE('1990-04-01'),
-    concat('email', num, '@example.com'),
-    LEFT(MD5(num::varchar), 100),
-    concat('address', num)
-  FROM generate_series(1, v_users_number) as num;
-  
-   INSERT INTO posts(author_id, title, content, status) SELECT
-    (num - 1) % v_users_number + 1,
-    LEFT(MD5(num::varchar), 5),
-    MD5(num::varchar),
-    'draft'
-  FROM generate_series(1, v_posts_for_each_user * v_users_number) as num;
-
-  UPDATE posts SET status = 'published' WHERE id % 10 = 0;
-
-  UPDATE posts SET tags = array_append(tags,'abc') WHERE id % 2 = 0;
-  UPDATE posts SET tags = array_append(tags,'def') WHERE id % 2 = 1;
-  UPDATE posts SET tags = array_append(tags,'ghk') WHERE id % 2 = 0;
-  UPDATE posts SET tags = array_append(tags,'lmn') WHERE id % 2 = 1;
-
-  INSERT INTO post_approvals(post_id, user_id, change) SELECT
-    (num - 1) % (v_posts_for_each_user - 1) + 1,
-    num,
-    1
-  FROM generate_series(1, v_users_number) as num;
-
-END $$;
-
-INSERT INTO post_approvals(post_id, user_id, change)
-VALUES (5, 1000, -1);
-
-INSERT INTO post_approvals(post_id, user_id, change)
-VALUES (5, 999, -1);
-
-INSERT INTO comments(user_id, post_id, content)
-VALUES (5, 1, '1'),
-        (5, 1, '2'),
-        (5, 1, '3');
-
-INSERT INTO comment_approvals(user_id, comment_id, change)
-VALUES (5, 1, 1),
-        (5, 2, -1),
-        (5, 3, 1);
-
--- add to post_editions
-DO $$
-  DECLARE v_users_number INT;
-  DECLARE v_posts_for_each_user INT;
-BEGIN
- v_users_number := 2000;
- v_posts_for_each_user := 50;
-
-  INSERT INTO post_editions(post_id, user_id, edited_at) SELECT
-    (num - 1) % (v_posts_for_each_user - 1) + 1,
-    (num - 1) % (v_users_number - 1) + 1,
-    DATE('2021-0' || num % 9 + 1 || '-' || num % 19 + 10)
-  FROM generate_series(1, v_users_number * 2) as num;
-
-END $$;
-
--- add to post_visits_per_day
-DO $$
-  DECLARE v_loop_amount INT;
-BEGIN
- v_loop_amount := 1000;
-
-  INSERT INTO post_visits_per_day(post_id, day_date, visits) SELECT
-    (num - 1) % (50 - 1) + 1,
-    DATE('2021-0' || num % 9 + 1 || '-' || num % 19 + 10),
-    num % 5 +  num % 10 
-  FROM generate_series(1, v_loop_amount) as num;
-
-END $$;
-
-SELECT * FROM posts LIMIT 10
+CREATE INDEX
+  ON posts USING BRIN (created_at);
--- src/psql/conftest.py
+++ src/psql/conftest.py
@@ -18,7 +18,7 @@ def engine():
 @pytest.fixture
 def inited_db(engine):
 
-    with open("architecture/task_2_architecture_without_data.sql", "r") as file_:
+    with open("architecture/task_2_architecture.sql", "r") as file_:
         query = file_.read()
 
     results = run_raw(engine, query)
@@ -29,10 +29,12 @@ def filled_db(inited_db, engine):
     Base = automap_base()
     Base.prepare(engine, reflect=True)
 
-    User = Base.classes.users
-
     session = Session(engine)
 
+    users_amount: int = 1000
+    posts_count: int = 2000
+
+    User = Base.classes.users
     session.bulk_save_objects(
         [
             User(
@@ -44,7 +46,22 @@ def filled_db(inited_db, engine):
                 password=f"password_{i}",
                 address=f"address_{i}",
             )
-            for i in range(100)
+            for i in range(users_amount)
+        ]
+    )
+
+    Post = Base.classes.posts
+    session.bulk_save_objects(
+        [
+            Post(
+                id=i,
+                author_id=i % users_amount,
+                title=f"title_{i}",
+                content=f"content_{i}",
+                created_at=f"20{random.randint(10,22):02}-{random.randint(1,12):02}-{random.randint(1,28):02}",
+                status=random.choice(["draft", "published", "archived"]),
+            )
+            for i in range(posts_count)
         ]
     )
     session.commit()
