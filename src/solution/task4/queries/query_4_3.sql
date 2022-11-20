56a852a330b1c7d0bef0fce97ca00fbbb51413b5
andrei.novoselov <andrei.novoselov@noveogroup.com>
2022-11-20 23:04:34 +0100

    feat: 4_3

--- src/solution/task4/queries/query_4_3.sql
+++ src/solution/task4/queries/query_4_3.sql
@@ -1,4 +1,4 @@
-SELECT posts.author_id, SUM(posts.rating) as summed_rating FROM posts
-GROUP BY posts.author_id
-ORDER BY SUM(posts.rating) DESC
+SELECT p.author_id, SUM(p.rating) as summed_rating FROM post p
+GROUP BY p.author_id
+ORDER BY SUM(p.rating) DESC
 LIMIT :N
