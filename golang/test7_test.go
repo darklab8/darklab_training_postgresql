68c7bec39e72ffd76e0e8a4ce7a43c9be1264f4f
anovoselov <andrei.novoselov@noveogroup.com>
2023-09-19 03:08:36 +0200

    feat: task 7

--- golang/test7_test.go
+++ golang/test7_test.go
@@ -1,6 +1,15 @@
 package golang
 
-import "darklab_training_postgres/golang/shared/utils"
+import (
+	"darklab_training_postgres/golang/shared"
+	"darklab_training_postgres/golang/shared/types"
+	"darklab_training_postgres/golang/shared/utils"
+	"database/sql"
+	"testing"
+
+	"github.com/uptrace/bun"
+	"gorm.io/gorm"
+)
 
 var (
 	Task7Query1 string
@@ -14,4 +23,25 @@ func init() {
 	Task7Query2 = utils.GetSQLFile(utils.ReadProjectFile("sql/task7/queries/query_7_2.sql"))
 	Task7Query3 = utils.GetSQLFile(utils.ReadProjectFile("sql/task7/queries/query_7_3.sql"))
 	Task7Query4 = utils.GetSQLFile(utils.ReadProjectFile("sql/task7/queries/query_7_4.sql"))
+}
+
+func TestTask7Query1(t *testing.T) {
+	shared.FixtureConn(TempDb.Dbname, func(dbname types.Dbname, conn *sql.DB, conn_orm *gorm.DB, bundb *bun.DB) {
+		result := conn_orm.Raw(Task7Query1)
+		utils.Check(result.Error)
+	})
+}
+
+func TestTask7Query2(t *testing.T) {
+	shared.FixtureConn(TempDb.Dbname, func(dbname types.Dbname, conn *sql.DB, conn_orm *gorm.DB, bundb *bun.DB) {
+		result := conn_orm.Raw(Task7Query2)
+		utils.Check(result.Error)
+	})
+}
+
+func TestTask7Query3(t *testing.T) {
+	shared.FixtureConn(TempDb.Dbname, func(dbname types.Dbname, conn *sql.DB, conn_orm *gorm.DB, bundb *bun.DB) {
+		result := conn_orm.Raw(Task7Query3)
+		utils.Check(result.Error)
+	})
 }
